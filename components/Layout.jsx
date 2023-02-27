0c16d1e9332cc6a06a9700c6b78e88c179daf2a3
aziz0519 <aziz2834@gmail.com>
2023-02-27 13:25:12 +0800

    changed NavBar path

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
-import NavBar from './Navbar'
-import Footer from './Footer'
+import { NavBar } from '.'
+import Footer from '.'
 
 const Layout = ({ children }) => {
   return (
